820a310285d6c895b733c6c47044952766c64c9c
yuta <yuta@macbookpro-yuta.local>
2018-11-01 19:03:45 +0900

    Contact completed message update.

--- src/AppBundle/Controller/ConfigController.php
+++ src/AppBundle/Controller/ConfigController.php
@@ -59,7 +59,7 @@ class ConfigController extends Controller
     }
     
     /**
-     * @Route("admin/setting/{slug}/config", name="setting_config", requirements={"slug"="register_email_subject|register_email|registered_description|cancel_email_subject|cancel_email|cancel_description|canceled_description"})
+     * @Route("admin/setting/{slug}/config", name="setting_config", requirements={"slug"="register_email_subject|register_email|registered_description|cancel_email_subject|cancel_email|cancel_description|canceled_description|contacted_description"})
      * @Method({"GET", "POST"})
      */
     public function configSettingAction(Request $request)
