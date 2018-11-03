5bf0da7d08a1392436d8adc6bf9d6503b4f91910
Yuta Konishi <info@kmusiclife.com>
2018-11-03 23:34:05 +0900

    Add get_template_directory_uri() helper function.

--- src/AppBundle/Twig/TwigExtension.php
+++ src/AppBundle/Twig/TwigExtension.php
@@ -33,9 +33,17 @@ class TwigExtension extends AbstractExtension
 	{
 	    return array(
 	        new \Twig_SimpleFunction('is_home', array($this, 'is_home')),
+	        new \Twig_SimpleFunction('get_template_directory_uri', array($this, 'get_template_directory_uri')),
 	        new \Twig_SimpleFunction('getSetting', array($this, 'getSetting')),
 	        new \Twig_SimpleFunction('getParameter', array($this, 'getParameter')),
 	    );
+	}
+	public function get_template_directory_uri()
+	{
+		if( $this->serviceContainer->getParameter('theme_name') ){
+			return '/themes/'.$this->serviceContainer->getParameter('theme_name');
+		}
+		return '/';
 	}
 	public function is_home($app)
 	{
